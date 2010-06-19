0a7ce2113414c31027ef45b1ce9b76eb29fda765
Miguel de Icaza <miguel@MonoMac2008.local>
2010-06-19 15:46:29 -0400

    Split the Twitter access routines from the database support code, to reuse for trivial apps that do not need the DB

--- TweetStation/Data/TwitterAccount.cs
+++ TweetStation/Data/TwitterAccount.cs
@@ -24,7 +24,6 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading;
-using SQLite;
 using MonoTouch.UIKit;
 using MonoTouch.Foundation;
 using System.Text;
@@ -41,7 +40,7 @@ namespace TweetStation
 		Transient,
 	}
 	
-	public class TwitterAccount
+	public partial class TwitterAccount
 	{
 		// The OAuth configuration for TweetStation
 		public static OAuthConfig OAuthConfig = new OAuthConfig () {
@@ -61,9 +60,6 @@ namespace TweetStation
 			
 		const string DEFAULT_ACCOUNT = "defaultAccount";
 		
-		[PrimaryKey, AutoIncrement]
-		public int LocalAccountId { get; set; }
-		
 		public long AccountId { get; set; }
 		public long LastLoaded { get; set; }
 		public string Username { get; set; }
@@ -73,56 +69,6 @@ namespace TweetStation
 		static NSString invoker = new NSString ("");
 		
 		static Dictionary<long, TwitterAccount> accounts = new Dictionary<long, TwitterAccount> ();
-		
-		public static TwitterAccount FromId (int id)
-		{
-			if (accounts.ContainsKey (id)){
-				return accounts [id];
-			}
-			
-			lock (Database.Main){
-				var account = Database.Main.Query<TwitterAccount> ("select * from TwitterAccount where LocalAccountId = ?", id).FirstOrDefault ();
-				if (account != null)
-					accounts [account.LocalAccountId] = account;
-				
-				return account;
-			}
-		}
-		
-		public static TwitterAccount Create (OAuthAuthorizer oauth)
-		{
-			var account = new TwitterAccount () {
-				Username = oauth.AccessScreenname,
-				AccountId = oauth.AccessId,
-				OAuthToken = oauth.AccessToken,
-				OAuthTokenSecret = oauth.AccessTokenSecret
-			};
-			lock (Database.Main)
-				Database.Main.Insert (account);
-			accounts [account.LocalAccountId] = account;
-			
-			return account;
-		}
-		
-		public static void Remove (TwitterAccount account)
-		{
-			var id = account.LocalAccountId;
-			bool pickNewDefault = id == Util.Defaults.IntForKey (DEFAULT_ACCOUNT);
-			
-			if (accounts.ContainsKey (id))
-				accounts.Remove (id);
-			
-			lock (Database.Main){
-				Database.Main.Execute ("DELETE FROM Tweet where LocalAccountId = ?", account.LocalAccountId);
-				Database.Main.Delete<TwitterAccount> (account);
-			
-				if (pickNewDefault){
-					var newDefault = Database.Main.Query<TwitterAccount> ("SELECT LocalAccountId FROM TwitterAccount WHERE OAuthToken != \"\"").FirstOrDefault ();
-					if (newDefault != null)
-						Util.Defaults.SetInt (newDefault.LocalAccountId, DEFAULT_ACCOUNT);
-				}
-			}
-		}
 		
 		public static TwitterAccount CurrentAccount { get; set; }
 		
@@ -358,74 +304,6 @@ namespace TweetStation
 				Console.WriteLine (e);
 			}
 			invoker.BeginInvokeOnMainThread (delegate { completed (urlToPic); });
-		}
-		
-		// 
-		// Posts the @contents to the @url.   The post is done in a queue
-		// system that is flushed regularly, so it is safe to call Post to
-		// fire and forget
-		//
-		public void Post (string url, string content)
-		{
-			var qtask = new QueuedTask () {
-				AccountId = LocalAccountId, 
-				Url = url, 
-				PostData = content,
-			};
-			lock (Database.Main)
-				Database.Main.Insert (qtask);
-			
-			FlushTasks ();
-		}
-		
-		void FlushTasks ()
-		{
-			lock (Database.Main){
-				var tasks = Database.Main.Query<QueuedTask> ("SELECT * FROM QueuedTask where AccountId = ? ORDER BY TaskId DESC", LocalAccountId).ToArray ();
-				ThreadPool.QueueUserWorkItem (delegate { PostTask (tasks); });
-			}
-		}
-		
-		// 
-		// TODO ITEMS:
-		//   * Need to change this to use HttpWebRequest, since I need to erad
-		//     the result back and create a tweet out of it, and insert in DB.
-		//
-		//   * Report error to the user?   Perhaps have a priority flag
-		//     (posts show dialog, btu starring does not?
-		//
-		// Runs on a thread from the threadpool.
-		void PostTask (QueuedTask [] tasks)
-		{
-			var client = GetClient ();
-			try {
-				Util.PushNetworkActive ();
-				foreach (var task in tasks){
-					Uri taskUri = new Uri (task.Url);
-					OAuthAuthorizer.AuthorizeRequest (OAuthConfig, client, OAuthToken, OAuthTokenSecret, "POST", taskUri, task.PostData);
-					try {
-						client.UploadData (taskUri, "POST", Encoding.UTF8.GetBytes (task.PostData));
-					} catch (Exception){
-						// Can happen if we had already favorited this status
-					}
-					
-					lock (Database.Main)
-						Database.Main.Execute ("DELETE FROM QueuedTask WHERE TaskId = ?", task.TaskId);
-				}
-			} catch (Exception e) {
-				Console.WriteLine (e);
-			} finally {
-				Util.PopNetworkActive ();
-			}
-		}
-		
-		public class QueuedTask {
-			[PrimaryKey, AutoIncrement]
-			public int TaskId { get; set; }
-			public long AccountId { get; set; }
-			public string Url { get; set; }
-
-			public string PostData { get; set; }
 		}
 	}
 	
