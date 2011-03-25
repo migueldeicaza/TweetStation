82f549687684bcf2e9c7b861b4061a8e58c401a7
Miguel de Icaza <miguel@gnome.org>
2011-03-24 22:14:18 -0400

    Refactored the code around a custom view controller

--- TweetStation/Utilities/Instapaper.cs
+++ TweetStation/Utilities/Instapaper.cs
@@ -32,8 +32,7 @@ namespace TweetStation
 
 			string username, password;
 			bool passwordError;
-			UINavigationController nav;
-			DialogViewController dvc;
+			InstapaperSignInController signinController;
 			
 			public InstapaperBookmark ()
 			{
@@ -60,107 +59,129 @@ namespace TweetStation
 				}
 			}
 			
-			public override bool LoggedIn {
-				get {
-					return username != null && password != null && passwordError == false;
-				}
-			}
-			
-			public override void SignIn (UIViewController container, Action<bool> done)
-			{
-				EntryElement ue, pe;
-				
-				var root = new RootElement ("Instapaper") {
-					new Section ("", "Get your account at instapaper.com"){
-						(ue = new EntryElement ("Username", "or email address", username)),
-						(pe = new EntryElement ("Password", "", password, true))
-					}
-				};
-				dvc = new DialogViewController (UITableViewStyle.Grouped, root, true);
-				nav = new UINavigationController (dvc);
-				
-				ue.BecomeFirstResponder (false);
-				
-				dvc.NavigationItem.SetLeftBarButtonItem (new UIBarButtonItem (Locale.GetText ("Close"), UIBarButtonItemStyle.Plain, delegate { Close (done, false);}), false);
-				dvc.NavigationItem.RightBarButtonItem = new UIBarButtonItem (Locale.GetText ("Save"), UIBarButtonItemStyle.Plain, delegate { Save (done, ue, pe); });
-
-				container.PresentModalViewController (nav, true);
-			}
-			
-			void Close (Action<bool> callback, bool done)
-			{
-				nav.DismissModalViewControllerAnimated (true);
-				nav = null;
-				dvc = null;
-				callback (done);
-			}
-			
-			ProgressHud hud;
-			UIAlertView alert;
-
-			void DestroyHud ()
-			{
-				hud.RemoveFromSuperview ();
-				hud = null;
-			}
-			
-			string MakeAuth (string user, string pass)
+			static string MakeAuth (string user, string pass)
 			{
 				return "Basic " + Convert.ToBase64String (Encoding.ASCII.GetBytes (user + ":" + pass));
 			}
 			
-			void Save (Action<bool> callback, EntryElement userElement, EntryElement passwordElement)
+
+			public override bool LoggedIn {
+				get {
+					return false && username != null && password != null && passwordError == false;
+				}
+			}
+			
+			public override void SignIn (UIViewController container, Action<bool> callback)
 			{
-				userElement.FetchValue ();
-				passwordElement.FetchValue ();
+				signinController = new InstapaperSignInController (this, result => {
+					signinController.Dispose ();
+					signinController = null;
+					callback (result);
+				});
 				
-				hud = new ProgressHud (Locale.GetText ("Authenticating"), Locale.GetText ("Cancel"));
-				hud.ButtonPressed += delegate {
-					DestroyHud ();
-					Close (callback, false);
-				};
-				dvc.View.AddSubview (hud);
+				container.PresentModalViewController (signinController, true);
+			}
+
+			// 
+			// Implements the view controller for doing the Instapaper sign-in
+			//
+			public class InstapaperSignInController : UINavigationController {
+				EntryElement userElement, passwordElement;
+				DialogViewController dvc;
+				InstapaperBookmark instapaper;
+				Action<bool> callback;
+				ProgressHud hud;
+				UIAlertView alert;
+	
+				public InstapaperSignInController (InstapaperBookmark instapaper, Action<bool> callback) 
+				{
+					this.instapaper = instapaper;
+					this.callback = callback;
+					
+					var root = new RootElement ("Instapaper") {
+						new Section ("", "Get your account at instapaper.com"){
+							(userElement = new EntryElement ("Username", "or email address", instapaper.username)),
+							(passwordElement = new EntryElement ("Password", "", instapaper.password, true))
+						}
+					};
+					dvc = new DialogViewController (UITableViewStyle.Grouped, root, true);
+					dvc.NavigationItem.SetLeftBarButtonItem (new UIBarButtonItem (Locale.GetText ("Close"), UIBarButtonItemStyle.Plain, delegate { Close (false);}), false);
+					dvc.NavigationItem.RightBarButtonItem = new UIBarButtonItem (Locale.GetText ("Save"), UIBarButtonItemStyle.Plain, delegate { Save (); });
+
+					SetViewControllers (new UIViewController [] { dvc }, false);
+					
+					userElement.BecomeFirstResponder (false);
+				}
 				
-				// Send an incomplete request with login/password, if this returns 403, we got the wrong password
-				ThreadPool.QueueUserWorkItem ((x) => {
-					try {
-						var req = (HttpWebRequest) WebRequest.Create (instapaperAddUri);
-						req.Method = "POST";
-						req.Headers.Add ("Authorization", MakeAuth (userElement.Value, passwordElement.Value));
+				void Close (bool done)
+				{
+					DismissModalViewControllerAnimated (true);
+					callback (done);
+				}
+				
+				void DestroyHud ()
+				{
+					hud.RemoveFromSuperview ();
+					hud = null;
+				}
+				
+				void Save ()
+				{
+					userElement.FetchValue ();
+					passwordElement.FetchValue ();
+					
+					hud = new ProgressHud (Locale.GetText ("Authenticating"), Locale.GetText ("Cancel"));
+					hud.ButtonPressed += delegate { DestroyHud (); };
+					dvc.View.AddSubview (hud);
+					
+					// Send an incomplete request with login/password, if this returns 403, we got the wrong password
+					ThreadPool.QueueUserWorkItem ((x) => { 
+						bool ok = false;
 						try {
-							req.GetResponse ();
-							nav.BeginInvokeOnMainThread (delegate { hud.Progress = 0.5f; });
-						} catch (WebException we){
-							var response = we.Response as HttpWebResponse;
-							if (response != null){
-								if (response.StatusCode == HttpStatusCode.Forbidden){
-									nav.BeginInvokeOnMainThread (delegate {
-										DestroyHud ();
-										alert = new UIAlertView (Locale.GetText ("Login error"), Locale.GetText ("Invalid password"), null, Locale.GetText ("Close"));
-										alert.WillDismiss += delegate { 
-											userElement.BecomeFirstResponder (true); 
-											alert = null; 
-										};
-										alert.Show ();
-									});
-									return;
-								}
-								Console.WriteLine ("Pased");
+							ok = ValidateCredentials (); 
+						} catch (Exception e){
+							Util.ReportError (this, e, "While validating credentials");
+						}
+						BeginInvokeOnMainThread (delegate {
+							DestroyHud ();
+							if (ok)
+								Close (true);
+						});
+					});
+				}
+				
+				bool ValidateCredentials ()
+				{
+					var req = (HttpWebRequest) WebRequest.Create (instapaperAddUri);
+					req.Method = "POST";
+					req.Headers.Add ("Authorization", MakeAuth (userElement.Value, passwordElement.Value));
+					try {
+						req.GetResponse ();
+						BeginInvokeOnMainThread (delegate { hud.Progress = 0.5f; });
+					} catch (WebException we){
+						var response = we.Response as HttpWebResponse;
+						if (response != null){
+							if (response.StatusCode == HttpStatusCode.Forbidden){
+								BeginInvokeOnMainThread (delegate {
+									DestroyHud ();
+									alert = new UIAlertView (Locale.GetText ("Login error"), Locale.GetText ("Invalid password"), null, Locale.GetText ("Close"));
+									alert.WillDismiss += delegate { 
+										userElement.BecomeFirstResponder (true); 
+										alert = null; 
+									};
+									alert.Show ();
+								});
+								return false;
 							}
 						}
-						// We got a valid password
-						Util.Defaults.SetString (username = userElement.Value, kUser);
-						Util.Defaults.SetString (password = passwordElement.Value, kPass);
-						passwordError = false;
-					} catch (Exception e){
-						Console.WriteLine ("Error: {0}", e);
 					}
-					nav.BeginInvokeOnMainThread (delegate {
-						DestroyHud ();
-						Close (callback, false);
-					});
-				});
-			}
+					// We got a valid password
+					Util.Defaults.SetString (instapaper.username = userElement.Value, kUser);
+					Util.Defaults.SetString (instapaper.password = passwordElement.Value, kPass);
+					instapaper.passwordError = false;
+					return true;
+				}
+			}			
 		}
 	}
 }
